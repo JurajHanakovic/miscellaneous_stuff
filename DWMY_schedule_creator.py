bc5529cd9a5244dd084adfdfaaa266d424c24cf8
Zen <juraj.hanakovic@hotmail.sk>
2025-07-10 05:02:26 +0200

    cleaning and removing unused stuff, adding some small stuff

--- DWMY_schedule_creator.py
+++ DWMY_schedule_creator.py
@@ -12,18 +12,14 @@ def generate_month_schedule(year, month):
 		list.append((date, day_name))
 	return list
 
+
 # User input for year and month
-# v1: no semi-weekly (or similar) tasks in schedule
 print (f"=======================================================================")
 print (f"= This script will generate daily schedule for given month.           =")
 print (f"= Exported .txt file with schedule will be saved to Downloads folder. =")
 print (f"=======================================================================")
 print (f"")
 year, month = map(int, input("Enter the year and month (YYYY MM): ").split())
-
-# v2: semi-weekly massage appointments currently postponed
-# User input for year and month and massage schedule
-# year, month, massage_next_week = map(int, input("Enter the year, month and wheather there is massage planned for next week (YYYY MM 0/1): ").split())
 
 
 # export print commands to external .txt file
@@ -63,107 +59,104 @@ for date, day_name in list:
 	############
 	# DAY-lies #
 	############
-	print(f"06:00  wake-up")
-	print(f"w_b, bat, prep_d (c, t, vw(vit+col))")
-	print(f"[c,t,vw] ms, mmed, DP&Q check")
-	print(f"bed tu, bat, tb, wght")
-	print(f"bb (hbb/cardio alternate) + [w]")
-	print(f"c_med (cl, ret, pa_succ, fu_succ, po_aff, cl)")
-	print(f"c_sho (+phcu), ec, ncl, deo, cr")
-	print(f"ChE_duct, ice_p")
-	print(f"|| work/DTD block + [w]; stretch ||")
-	print(f"12:00  [w&lunch] + soc/quiet, tb/chg")
-	print(f"powernap (10-30min)")
-	print(f"|| work/DTD block + [w]; stretch ||")
-	print(f"15:00  [prot/(de)c/t+snack/protBar], tb/chg")
-	print(f"|| work/DTD block + [w]; stretch ||")
-	print(f"18:00  [w&din] + soc/tv, tb/chg")
-	print(f"|| relax/DTD block + [w]; sg ||")
-	print(f"mmed, bat, tb, deo")
-	print(f"charge mp, adb + sleep")
-	print(f"======")
+# TODO: rework with basic morning (&evening? &during_day?) routine
+	# not-work-day schedule
+	# print(f"05:00  wake-up", w_b, bat)
+	# print(f"weight, prep_d (c, t, vw(vit+col+cr), 1.5l wb)")
+	# print(f"[c/t,vw] zt - ms, (zenaf
+	# print(f"z2 - mmed, DP&Q check")
+	# print(f"bed tu, bat, tb, wght")
+	# print(f"bb (hbb/cardio alternate) + [w]")
+	# print(f"c_med (cl, ret, pa_succ, fu_succ, po_aff, cl)")
+	# print(f"c_sho (+phcu), ec, ncl, deo, cr")
+	# print(f"ChE_duct, ice_p")
+	# print(f"|| work/DTD block + [w]; stretch ||")
+	# print(f"11:00  [w&lunch] + soc/quiet, tb/chg")
+	# print(f"powernap (10-30min)")
+	# print(f"|| work/DTD block + [w]; stretch ||")
+	# print(f"14:00  [prot/(de)c/t+snack/protBar], tb/chg")
+	# print(f"|| work/DTD block + [w]; stretch ||")
+	# print(f"17:00  [w&din] + soc/tv, tb/chg")
+	# print(f"|| relax/DTD block + [finish_wb]; sg ||")
+	# print(f"mmed, bat, tb, deo")
+	# print(f"charge mp, adb + sleep")
+	# print(f"======")
 
 	
 	#############
 	# WEEK-lies #
 	#############
-	# 17:00 massage (every other week) // currently postponed
-	# if day_name == "Thursday" and massage_next_week == 1:
-	#     print(f"* 17:00 massage")
-	#     massage_next_week = 0
-	# elif day_name == "Thursday":
-	#     massage_next_week = 1
-
-	# EGSfg
+	# EGSfg PC&Android
 	if day_name == "Thursday":
-		print(f"* EGS free games (17:00+)")
+		print(f"'EGS free games PC+Android (17:00+)")
 
 	# clean office space & peripherals // currently postponed
-	# if day_name == "Friday":
-	# 	print(f"* office space & peripherals cleaning")
+	if day_name == "Friday":
+		print(f"'office space & peripherals cleaning")
+		print(f"^get&note info about next week's shifts")
 
 	# sunday cleaning (personal room/office & body)
 	if day_name == "Sunday":
-		print(f"* personal room/office cleaning")
-		print(f"	vacuum, dust, peripherals, trash out")
-		print(f"* fbhcu, wh bath/shower, nc, ec, fbcr")
+		print(f"'fast flat & my_office cleaning")
+		print(f"'fbhcu, wh bath/shower, nc, ec, fbcr")
 
 
 	##############
 	# MONTH-lies #
 	##############
-	# invoice
-	# if int(date[:2]) == 1:
-	# 	print(f"* create & send invoice")
-
 	# UEfreebies (1st Thursday after 7th of each month)
 	if day_name == "Thursday" and (6 < int(date[:2]) < 14):
-		print("* UE monthly freebies")
+		print("'UE monthly freebies")
 
 	# cashflow payments
-	if int(date[:2]) == 15:
-		print(f"* cashflow payments")
-		print(f"	rent")
+	if int(date[:2]) == 20:
+		print(f"^cashflow payments")
 
 	# 3rd last day of the month tasks
 	if int(date[:2]) == num_days-2:
-		print(f"* big flat cleaning")
-		print(f"	vacuum, dust, karim, ...")
-		print(f"* big pc&peripherals cleaning")
-		print(f"	displays, keyboards, mice, gamepads, pc fans, ...")
+		print(f"'big flat tu")
+		print(f"	dust, mop/vacuum, trash, ...")
+		print(f"'big pc&peripherals cleaning")
+		print(f"	displays, keyboards, mice, gamepads, ...")
 
 	# 2nd last day of the month tasks
 	if int(date[:2]) == num_days-1:
-		print(f"* relax & regeneration")
+		print(f"%R&R")
 
 	# last day of the month tasks
 	if int(date[:2]) == num_days:
-		print(f"* bb progress")
+		print(f"%bb progress")
 		print(f"	weighing; photos before training")
 		print(f"	training - check exercises progress")
 		print(f"	photos after training; photos save & rip")
-		print(f"* review big quests/projects & prep calendar for next month")
-		print(f"	DR, Habits, BB, FF")
+		print(f"'review big quests/projects & prep new schedule")
+		print(f"	DR, Habits, BB, FF, ...")
 
 
 	#############
 	# YEAR-lies #
 	#############
-	# best-of last year stuff check & misc (2nd saturday of january; after 15th)
-	if month == 1 and day_name == "Saturday" and (15 < int(date[:2]) < 23):
-		print(f"* check on YT&/Google")
+	# best-of last year stuff check & misc (2nd/3rd saturday of january; after 14th)
+	if month == 1 and day_name == "Saturday" and (14 < int(date[:2]) < 23):
+		print(f"'check on YT&/Google")
 		print(f"	best pc games of ...")
 		print(f"	best tv shows of ...")
 		print(f"	best movies of ...")
 		print(f"	best of year ...")
-		print(f"(* 2nd SIM -- refill credit")
+		print(f"('Predplatenka Telekom -- refill credit")
+		print(f"('check/change clothes in car trunk")
+
+	# mid-year misc (2nd/3rd saturday of july; after 14th)
+	if month == 1 and day_name == "Saturday" and (14 < int(date[:2]) < 23):
+		print(f"('Predplatenka Telekom -- refill credit")
 
 	# end of the year personal stuff recap & prep for new year (31.12.)
 	if int(date[:2]) == 31 and month == 12:
-		print(f"* check & update big Plan-X quests")
-		print(f"* personal yearly retrospective session")
+		print(f"'check & update big PlanX quests")
+		print(f"'med - personal yearly retrospective session")
 
 	
 	# spacing
+	print(f"")
 	print(f"")
 	print(f"")
